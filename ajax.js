917496bcc06c975dd35386bd3489131b70b4746b
heimusu <heimusu@gmail.com>
2014-03-31 01:40:25 +0900

    success indicate table

--- ajax.js
+++ ajax.js
@@ -2,24 +2,35 @@ $( function(){
     $('#ajax-button').click(
 	function(){
 	    $.ajax({
-		url:'http://tutujibus.com/busstopLookup.php?rosenid=1&callback=?',
+		url:'http://tutujibus.com/timetableLookup.php?rosenid=1&callback=?',
 		type:'GET',
 		dataType:'jsonp',
 		jsonp:'callback',
 		success:function(data){
 		    alert("ok");
 		    console.log(data);
+
+		    /*data convert for text*/
 		    /*var res = JSON.stringify(data);
 		    console.log(res);*/
-		    //alert(data.busstop[0].id);
-		    for(var i=0;i<44;i++){
-			var count=0;
-			count += data.busstop[i].id;
+		    /*alert(data.timetable[0].destination);
+		    alert(data.timetable[0].list[0].time);*/
+		    
+		    
+		    for(var i=0;i<19;i++){
+			var arraylistlength = data.timetable[i].list.length;
+			for(var j=0;j<arraylistlength;j++){
+			    if(data.timetable[i].list[j].busstopid == 1){
+				console.log('%s,%s,%s',data.timetable[i].binid,data.timetable[i].list[j].time,data.timetable[i].destination);
+			    }
+			}
 		    }
-		    alert(count);
+
+		    alert("end");
+
 		},
 		error:function(data){
-		    alert("ng");
+		    alert("Access NG");
 		},
 		complete:function(data){
 		    alert("complete");
