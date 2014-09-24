bacec8897adb9014c7842f3a1839ea4e28f33ed0
heimusu <heimusu@gmail.com>
2014-09-24 19:19:26 +0900

    stopTime.jsの導入（非同期実行のため，stopTime.jsが現在時刻を正しく取得できていない)

--- golibrary/stopTime.js
+++ golibrary/stopTime.js
@@ -1,71 +1,77 @@
 /*残り時間をカウントする*/
+$(function(){
+    $.extend(this,window.top.init);
+    countDown();
+});
+
 function countDown(){
+    $.extend(this,window.top.init);
     var lefthour=0;
 	var leftminute=0;
 	var nowtime = new Date();
-	var nowHour = hour2;//nowtime.getHours();
-	var nowMinute = minute2;//nowtime.getMinute();;
+	var nowHour = this.hour2;//nowtime.getHours();
+	var nowMinute = this.minute2;//nowtime.getMinute();;
 	var nexthour=0;
 	var nextminute=0;
 	var a_day = 24 * 60 * 60 * 1000;
 	/*バスが一時間以内にくる場合*/
-	if(flag1 == 1){
+	if(this.flag1 == 1){
 	/*同じ時間帯にバスが来る（例：現在8時，次のバスは8時台）の場合*/
-	    if(flag2 == 1){
+	    if(this.flag2 == 1){
 		    lefthour = 0;
-			leftminute = res2 - minute2;
+			leftminute = this.res2 - this.minute2;
 		}
-		else if(res1 - hour2 == 2){
+		else if(this.res1 - this.hour2 == 2){
 		    lefthour = 1;
-			leftminute = res2 + 60 - minute2;
-			if(minute2 <= res2){
+			leftminute = this.res2 + 60 - this.minute2;
+			if(this.minute2 <= this.res2){
 		        lefthour = 2;
 				leftminute = res2 - minute2;
 			}
 		}
-		else if(hour2 < res1){
-		    lefthour = res1 - hour2;
-			leftminute = res2 - minute2;
-			if(minute2 > res2){
+		else if(this.hour2 < this.res1){
+		    lefthour = this.res1 - this.hour2;
+			leftminute = this.res2 - this.minute2;
+			if(this.minute2 > this.res2){
 				lefthour -= 1;
 				leftminute += 60;
 			}
 		}
 		/*次の時間帯にバスが来る（例：現在8時台，次のバスは9時台）の場合*/
 		else{
-			if((res2 - minute2) < 0){
+			if((this.res2 - this.minute2) < 0){
 			lefthour = 0;
-			leftminute = res2 + 60 - minute2;
-			if(hour2 < starthour || hour2 != res1){
-				lefthour = res1 - hour2;
-				if(minute2 > res2){
+			leftminute = this.res2 + 60 - this.minute2;
+			if(this.hour2 < starthour || this.hour2 != this.res1){
+				lefthour = this.res1 - this.hour2;
+				if(this.minute2 > this.res2){
 				lefthour = 0;
 				}
 			}
 			}
 			else{
-			    lefthour = res1 - hour2;
-				leftminute = res2 - minute2;
+			    lefthour = this.res1 - this.hour2;
+				leftminute = this.res2 - this.minute2;
 			}
 		}
 	}
 	/*次のバスまで一時間以上ある場合*/
-	else if(flag3 == 1){
-	    tmp = res3 - hour2;
+	else if(this.flag3 == 1){
+	    this.tmp = this.res3 - this.hour2;
 		lefthour = 1;
-		leftminute = res4 - minute2;
+		leftminute = this.res4 - this.minute2;
 		if(minute2 > res4){
 		    lefthour = 0;
-			leftminute = res4 + 60 - minute2;
-			if(tmp != 1){
+			leftminute = this.res4 + 60 - this.minute2;
+			if(this.tmp != 1){
 				lefthour = 1;
 			}
 		}
 	}
 	/*次のバスまで2時間以上ある場合*/
-	else if(flag4 == 1){
+	else if(this.flag4 == 1){
 	    lefthour = 2;
-		leftminute = res2 - minute2;
+		leftminute = this.res2 - this.minute2;
 	}
 	
     /*バスが来る場合，残り時間を表示*/
--- golibrary/timeTable.js
+++ golibrary/timeTable.js
@@ -1,6 +1,5 @@
-window.top.timeTable= function(){
-
-var init = {
+//初期変数
+window.top.init = {
     hour1 : 0,
 	minute1 : 0,
 	hour2 : 0,
@@ -20,10 +19,12 @@ var init = {
 	flag7 : 0,
 	tmp : 0,
 	hash : location.hash.substring(1)
-	//hash = hash.substring(1);
 }
 
-var timeTable =  function(){
+
+
+//時刻表・バス時刻サジェスト
+$(function(){
 	$.ajax({
 		url:'http://tutujibus.com/timetableLookup.php?rosenid=1&callback=?',
 		type:'GET',
@@ -31,24 +32,27 @@ var timeTable =  function(){
 		jsonp:'callback',
 		success:function(data){
             $.extend(this,init);
-			/*table variable*/
+			/*時刻表描画用変数*/
 			var length = data.timetable.length;
 			var table;
 			var row_no=1;
 			
-			/*time variable*/
-			/*busstop time array*/
+			/*到着時刻表示用変数*/
 			var k = 0;
 			var l = 0;
 			var bustime = new Array();
 			var bin_num = new Array();
 			var arriveTime;
-			/*nowtime*/
+			
+            /*現在時刻*/
 			this.hour2 = this.DD.getHours();
 			this.minute2 = this.DD.getMinutes();
 			
+            //デバッグ用設定
+            this.hour2 = 16;
+            this.minute2 = 0;
 
-			/*main function*/
+			/*時刻表を出力*/
 			for(var i=0;i<length;i++){
 			    var arraylistlength = data.timetable[i].list.length;
 			    for(var j=0;j<arraylistlength;j++){
@@ -69,23 +73,18 @@ var timeTable =  function(){
 			    }
 			}
 
-			/*debug code*/
-			/*hour2 = 16;
-			minute2 = 55;*/
-
-			/*indicate next time*/
-			/*hour2 & minute2 = nowtime, hour1&minute1 = next time*/
+			/*次のバスの発車時刻を表示*/
+			/*hour2 & minute2 = 現在時刻, hour1&minute1 = 次のバスの発車時刻*/
 			var starthour = Number(bustime[0][0] + bustime[0][1]);
 			var lasthour = Number(bustime[k-1][0] + bustime[k-1][1]);
-			//console.log(starthour,lasthour);
 			for(i=0;i<k;i++){
-			    hour1 = Number(bustime[i][0] + bustime[i][1]);
-			    minute1 = Number(bustime[i][3] + bustime[i][4]);
-			    comp1 = hour1 - this.hour2;
+			    this.hour1 = Number(bustime[i][0] + bustime[i][1]);
+			    this.minute1 = Number(bustime[i][3] + bustime[i][4]);
+			    this.comp1 = this.hour1 - this.hour2;
 			    /*始発より早い段階で閲覧した場合*/
 			    if(this.hour2 < starthour){
-				res1 = starthour;
-				res2 = Number(bustime[0][3] + bustime[0][4]);
+				this.res1 = starthour;
+				this.res2 = Number(bustime[0][3] + bustime[0][4]);
 				this.flag1 = 1;
 			    }
 			    /*終発より後の段階で閲覧した場合*/
@@ -93,113 +92,108 @@ var timeTable =  function(){
 				this.flag5 = 1;
 			    }
 			    /*バスが一時間以内にくる場合*/
-			    else if(comp1 == 0 && this.flag1 == 0 && this.flag5 == 0){
+			    else if(this.comp1 == 0 && this.flag1 == 0 && this.flag5 == 0){
 				/*次の時間帯に来る（例：8時現在，次のバスは9時台)場合*/
 				if(this.minute2 > this.minute1 && i != k-1 || this.minute2 == this.minute1 && i != k-1){
-				    res1 = Number(bustime[i+1][0] + bustime[i+1][1]);
-				    res2 = Number(bustime[i+1][3] + bustime[i+1][4]);
+				    this.res1 = Number(bustime[i+1][0] + bustime[i+1][1]);
+				    this.res2 = Number(bustime[i+1][3] + bustime[i+1][4]);
 				    if(this.minute2 == this.minute1){
-					res3 = res1;
-					res4 = res2;
-					flag3 = 1;
+					this.res3 = this.res1;
+					this.res4 = this.res2;
+					this.flag3 = 1;
 				    }
-				    else if(this.minute2 > this.minute1 && res1 == this.hour2){
-					res2 = Number(bustime[i][3] + bustime[i][4]);
+				    else if(this.minute2 > this.minute1 && this.res1 == this.hour2){
+					this.res2 = Number(bustime[i][3] + bustime[i][4]);
 				    }
 				    else{
-					flag1 = 1;
+					this.flag1 = 1;
 				    }
 				    
 				}
 				/*次のバスがもう無い場合*/
 				else if(i == k-1){
-				    if(minute2 < minute1){
-					res1 = lasthour;
-					res2 = Number(bustime[k-1][3] + bustime[k-1][4]);
-					flag1 = 1;
+				    if(this.minute2 <this. minute1){
+					this.res1 = lasthour;
+					this.res2 = Number(bustime[k-1][3] + bustime[k-1][4]);
+					this.flag1 = 1;
 				    }
 				    else{
-					flag5 = 1;
+					this.flag5 = 1;
 				    }
 				}
 				/*次のバス（例：8時現在，次のバスは8時台）が来る場合*/
 				else{
-				    res1 = hour1;
-				    res2 = minute1;
-				    flag1 = 1;
-				    flag2 = 1;
+				    this.res1 = this.hour1;
+				    this.res2 = this.minute1;
+				    this.flag1 = 1;
+				    this.flag2 = 1;
 				}
 			    }
 			    /*バスが来るまで一時間以上ある場合*/
-			    else if(comp1 == 1 && flag1 == 0 && flag2 == 0 && flag3 == 0 && flag5 == 0){
-				//res3 = Number(bustime[i+1][0] + bustime[i+1][1]);
-				//res4 = Number(bustime[i+1][3] + bustime[i+1][4]);
-				res3 = Number(bustime[i][0] + bustime[i][1]);
-				res4 = Number(bustime[i][3] + bustime[i][4]);
-				flag3 = 1;
+			    else if(this.comp1 == 1 && this.flag1 == 0 && this.flag2 == 0 && this.flag3 == 0 && this.flag5 == 0){
+				this.res3 = Number(bustime[i][0] + bustime[i][1]);
+				this.res4 = Number(bustime[i][3] + bustime[i][4]);
+				this.flag3 = 1;
 			    }
 			    /*バスが来るまで二時間以上ある場合*/
-			    else if(comp1 == 2 && flag1 == 0 && flag2 == 0 && flag3 == 0 && flag4 == 0 && flag5 == 0){
-				res3 = Number(bustime[i+2][0] + bustime[i+2][1]);
-				res4 = Number(bustime[i+2][3] + bustime[i+2][4]);
-				flag4 = 1;
+			    else if(this.comp1 == 2 && this.flag1 == 0 && this.flag2 == 0 && this.flag3 == 0 && this.flag4 == 0 && this.flag5 == 0){
+				this.res3 = Number(bustime[i+2][0] + bustime[i+2][1]);
+				this.res4 = Number(bustime[i+2][3] + bustime[i+2][4]);
+				this.flag4 = 1;
 			    }
 			}
 			/*バスが来ない場合*/
-			if(flag1 == 0 && flag2 == 0 && flag3 == 0 && flag4 == 0){
-			    flag5 = 1;
+			if(this.flag1 == 0 && this.flag2 == 0 && this.flag3 == 0 && this.flag4 == 0){
+			    this.flag5 = 1;
 			}
 
-
-			//console.log(flag1,flag2,flag3,flag4,flag5,flag6);
-			//console.log(res1,res2,res3,res4);
 			/*フラグを基に表示する時刻を判定*/
 			/*次のバスが1時間以内にくる場合*/
-			if(flag1 == 1 && this.flag3 == 0 || flag2 == 1 && res2 >= 0){
-			    $("#message").html("次のバスは"+res1+"時"+res2+"分発車です");
-			    flag6 = 1;
+			if(this.flag1 == 1 && this.flag3 == 0 || this.flag2 == 1 && this.res2 >= 0){
+			    $("#message").html("次のバスは"+this.res1+"時"+this.res2+"分発車です");
+			    this.flag6 = 1;
 			}
 			/*次のバスまで1時間以上ある場合*/
-			else if(flag3 == 1 && res3 >= 0 && res4 >= 0){
-			    $("#message").html("次のバスは" + res3 + "時" + res4 + "分発車です");
-			    flag7 = 1;
+			else if(this.flag3 == 1 && this.res3 >= 0 && this.res4 >= 0){
+			    $("#message").html("次のバスは" + this.res3 + "時" + this.res4 + "分発車です");
+			    this.flag7 = 1;
 			}
 			/*次のバスまで2時間以上ある場合*/
-			else if(flag4 == 1 && res3 >= 0 && res4 >= 0){
-			    $("#message").html("次のバスは" + res3 + "時" + res4 + "分発車です");
-			    flag7= 1;
+			else if(this.flag4 == 1 && this.res3 >= 0 && this.res4 >= 0){
+			    $("#message").html("次のバスは" + this.res3 + "時" + this.res4 + "分発車です");
+			    this.flag7= 1;
 			}
 			/*バスがもう無い場合*/
-			else if(res3 < 0 || flag5 == 1){
+			else if(this.res3 < 0 || this.flag5 == 1){
 			    $("#message").html("バスはしばらくこないようです");
 			}
-			//console.log(Number(data.timetable[0].list[0].time[0] + data.timetable[0].list[0].time[1]));
 			//乗車したバスの便番号を取得
 			var checkbin = 0; //取得した発車時刻が他の便の図書館到着と一致するケースがあるため，誤検出を防ぐ
 			for(i=0;i<length;i++){
 			    arraylistlength = data.timetable[i].list.length;
 			    for(j=0;j<arraylistlength;j++){
-				if(flag6 == 1){
+				if(this.flag6 == 1){
 				    var tmpHour = Number(data.timetable[i].list[j].time[0] + data.timetable[i].list[j].time[1]);
 				    var tmpMinute = Number(data.timetable[i].list[j].time[3] + data.timetable[i].list[j].time[4]);
-				    if(res1 == tmpHour && res2 == tmpMinute && checkbin == 0){
+				    if(this.res1 == tmpHour && this.res2 == tmpMinute && checkbin == 0){
 					var hitNum = Number(data.timetable[i].binid);
 					checkbin = 1;
 				    }
 				}
-				else if(flag7 == 1){
+				else if(this.flag7 == 1){
 				    var tmpHour = Number(data.timetable[i].list[j].time[0] + data.timetable[i].list[j].time[1]);
 				    var tmpMinute = Number(data.timetable[i].list[j].time[3] + data.timetable[i].list[j].time[4]);
-				    if(res3 == tmpHour && res4 == tmpMinute && checkbin == 0){
+				    if(this.res3 == tmpHour && this.res4 == tmpMinute && checkbin == 0){
 					var hitNum = Number(data.timetable[i].binid);
 					checkbin = 1;
 				    }
 				}
 			    }
 			}
-			//console.log(hitNum);
-			var hitflag = 0;
+			
+            var hitflag = 0;
 			//該当便の時刻表を取得し，図書館にいつ着くのかを調べる
+            //便番号の取得状態では，エラーを発生させる可能性がある(未修正)(便番号1,2)
 			for(i=0;i<length;i++){
 			    arraylistlength = data.timetable[i].list.length;
 			    for(j=0;j<arraylistlength;j++){
@@ -213,11 +207,11 @@ var timeTable =  function(){
 				    tmpHour = Number(data.timetable[i].list[j].time[0] + data.timetable[i].list[j].time[1]);
 				    tmpMinute = Number(data.timetable[i].list[j].time[3] + data.timetable[i].list[j].time[4]);
 				    //乗車するバス停の，乗車した便によって図書館へ行かない場合がある
-				    if(res2 > tmpMinute || res1 > tmpHour && res2 < tmpMinute){
+				    if(this.res2 > tmpMinute || this.res1 > tmpHour && this.res2 < tmpMinute){
 					$('#arrivetime').html("このバスは図書館へは参りません");
 					hitflag = 0;
 				    }
-				    else if(res4 > tmpMinute || res3 > tmpHour && res4 < tmpMinute){
+				    else if(this.res4 > tmpMinute || this.res3 > tmpHour && this.res4 < tmpMinute){
 					$('#arrivetime').html("このバスは図書館へは参りません");
 					hitflag = 0;
 				    }
@@ -230,7 +224,7 @@ var timeTable =  function(){
 				    tmpHour = Number(data.timetable[i].list[j].time[0] + data.timetable[i].list[j].time[1]);
 				    tmpMinute = Number(data.timetable[i].list[j].time[3] + data.timetable[i].list[j].time[4]);
 				    //乗車するバス停の，乗車した便によって図書館へ行かない場合がある
-				    if(res2 > tmpMinute || res1 > tmpHour && res2 < tmpMinute){
+				    if(this.res2 > tmpMinute || this.res1 > tmpHour && this.res2 < tmpMinute){
 					$('#arrivetime').html("このバスは図書館へは参りません");
 					hitflag = 0;
 				    }
@@ -247,7 +241,7 @@ var timeTable =  function(){
 			    $('#arrivetime').html("このバスは図書館へは参りません");
 			}
 
-			if(this.hash == 20 || this.hash == 28){
+			if(this.hash == 20 || this.hash == 28 || this.flag5 == 1){
 			    $('#arrivetime').html(" ");
 			}
         },
@@ -256,7 +250,5 @@ var timeTable =  function(){
 		    alert("Access NG");
 		}
 	});
-}
+});
 
-timeTable();
-}();
