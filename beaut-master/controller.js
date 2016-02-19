20651adb7352ce9aa74d48186d79bf52e199fcda
heimusu <heimusu@gmail.com>
2016-02-19 17:45:53 +0900

    json取得と表示成功．this利用は失敗．情報が少ないのでとりあえず保留

--- beaut-master/controller.js
+++ beaut-master/controller.js
@@ -1,12 +1,24 @@
 angular.module('indexMod', [])
   .controller('indexController', ['$scope', '$http', function($scope, $http){
 
-    this.test = function(){
-      $http.get('http://127.0.0.1:3000/dummy.json'
+    //ボタン押下でデータ取得
+    /*
+    $scope.test = function(){
+      //$http.get('http://127.0.0.1:3000/dummy.json'
+      $http.get('./dummy.json'
       ).success(function(data){
-        this.result = data;
+        $scope.result = data;
       })
       .error(function(data,status,headers,config){
+        console.log(error);
       });
     };
+    */
+    $http.get('./dummy.json'
+    ).success(function(data){
+      $scope.result = data;
+    })
+    .error(function(data,status,headers,config){
+      console.log(error);
+    });
 }]);
