c495392bbc80b0c9583963ca603eddfdc352c9d7
heimusu <heimusu@gmail.com>
2016-03-31 08:57:20 +0900

    少し手直し

--- beaut-master/controller.js
+++ beaut-master/controller.js
@@ -180,24 +180,6 @@ module.controller('indexController', ['$scope', '$http', '$window', '$location',
     };
 
 
-
-    // upload on file select or drop
-    /*
-    $scope.upload = function (file) {
-        Upload.upload({
-            url: 'upload/url',
-            data: {file: file, 'username': $scope.username}
-        }).then(function (resp) {
-            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
-        }, function (resp) {
-            console.log('Error status: ' + resp.status);
-        }, function (evt) {
-            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-        });
-    };
-    */
-
     $scope.upload = function (file) {
         Upload.upload({
             url: 'http://private-cb543-beautapiv1.apiary-mock.com/v1/images',
@@ -212,21 +194,6 @@ module.controller('indexController', ['$scope', '$http', '$window', '$location',
             console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
         });
     };
-
-    /*
-    $scope.upload = function (file) {
-        Upload.upload({
-            url: 'http://private-cb543-beautapiv1.apiary-mock.com/v1/images',
-            data: {file: file}
-        })
-        .success(function(data, status, headers, config){
-            console.log(data);
-        })
-        .error(function(data, status, headers, config){
-            console.log(data);
-        });
-    };
-    */
 
 
 }]);
